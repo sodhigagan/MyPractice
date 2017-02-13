96f95fa3101ced098afc2ab25db5170c765e9230
sodhigagan <sodhigagan@gmail.com>
2017-02-13 00:00:37 -0500

    there can be input with fmt.Scan :)

--- AnyNameInBinary/main.go
+++ AnyNameInBinary/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
+	var L1 string
 	fmt.Println("Enter your name:")
-	letter := bufio.NewScanner(os.Stdin)
-	letter.Scan()
-	a := letter.Text()
-	fmt.Println("So your name is: ", a) //, letter.Text())
-	//fmt.Printf("%b", a)
+	fmt.Scan(&L1)
+	fmt.Println("nice to meet you", L1)
 }
